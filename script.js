9bb6647fb0a31eb76db9dba59c71e91fd9b47dee
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2024-06-04 23:38:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const closeBtn = document.getElementById("close-btn")
+const modal = document.getElementById("modal")
+const openBtn = document.getElementById("openModal")
+closeBtn.addEventListener("click",()=>{
+	
+	modal.classList.add("display-modal")
+})
+openBtn.addEventListener("click",()=>{
+	modal.classList.remove("display-modal")
+})
+
+modal.addEventListener('click', ()=>{
+	modal.classList.add("display-modal")
+})
